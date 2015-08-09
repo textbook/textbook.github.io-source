b951f8c6c480d4cb04ff5bebe3e8682bc92a769f
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2015-08-09 21:38:24 +0100

    Dogfooding pelican_scaffold

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+
+#basic setttings
+GH_USERNAME=textbook
+
+#advanced settings
 BRANCH=master
-TARGET_REPO=textbook/textbook.github.io.git
+GITHUB_OUTPUT_FOLDER=built_website
 PELICAN_OUTPUT_FOLDER=output
+SHOW_CHANGES=true
+TARGET_REPO=$GH_USERNAME/$GH_USERNAME.github.io.git
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
+    echo -e "Starting to deploy to GitHub Pages\n"
     if [ "$TRAVIS" == "true" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
@@ -12,17 +19,20 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     # Pull hash and commit message of the most recent commit
     commitHash=$(git rev-parse HEAD)
     commitMessage=$(git log -1 --pretty=%B)
-    #go into directory and authenticate to remote
-    cd $PELICAN_OUTPUT_FOLDER
-    git checkout -b temp
-    git remote rm origin
-    git remote add origin https://${GH_PAGES}@github.com/$TARGET_REPO
+    #get the current output in a separate directory
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO $GITHUB_OUTPUT_FOLDER > /dev/null
+    #copy the new output
+    cd $GITHUB_OUTPUT_FOLDER
+    rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
     #add, commit and push files
-    git add -f -A .
-    echo -e "Changes:\n"
-    git status -s
-    git commit -m "$commitMessage" -m "Commit $commitHash pushed to GitHub Pages by Travis build $TRAVIS_BUILD_NUMBER"
-    git checkout -B master temp
+    git add -A .
+    if [ "$SHOW_CHANGES" == "true" ]; then
+      echo -e "Changes:"
+      git status -#!/bin/sh
+      echo -e "\n"
+    fi
+    detailedMessage="Commit $commitHash pushed to GitHub Pages by Travis build $TRAVIS_BUILD_NUMBER"
+    git commit -m "$commitMessage" -m "$detailedMessage"
     git push -fq origin $BRANCH > /dev/null
     echo -e "Deploy completed\n"
 fi
